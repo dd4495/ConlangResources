80dac55c711db1e42ddce4320751284f7855a4d4
dd4495 <dd4495@users.noreply.github.com>
2016-08-04 10:51:47 -0400

    Added Transform-Words script and Readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Pre-requisites #
+
+* Windows PC running one of the following:
+	* Windows 7
+	* Windows 8/8.1
+	* Windows 10
+
+
+# Instructions #
+
+* Open your Start menu and type 'Powershell' (without quotes).
+* Click either the **Powershell** or **Powershell ISE** icon. 
+* After Powershell opens, type "cd C:\\*savefolder*" where *savefolder* is the folder in which you saved the script.
+	* If your browser automatically saves downloaded files, the script will likely be in your Downloads folder.
+* In the Powershell window, type ".\Transform-Words -BaseWords kit, kat -Endings ik, ak, ok" (without quotes) and press 'Enter'
+	* You should see a table that looks like this:
+	
+		|  ik   | ak    | ok    |
+		|-------|-------|-------|
+		| kitik | kitak | kitok |
+		| katik | katak | katok |
+
+* Now, just replace the base words and endings with your own! 
---
+++ Transform-Words.ps1
@@ -0,0 +1,44 @@
+﻿#requires -Version 2
+
+ <#
+.SYNOPSIS
+  Transforms words by adding a suffix 
+.DESCRIPTION
+  This script will transform a word or list of words based on the supplied suffix(es).
+.PARAMETER BaseWords
+    This is the root word or list of root words. This parameter can accept either 
+    a single entry or a comma-separated list of entries
+.PARAMETER Endings
+    This is the suffix or list of suffixes. This parameter can accept either 
+    a single entry or a comma-separated list of entries
+.EXAMPLES
+    .\Transform-Words.ps1 -Endings ik, ak, ok -BaseWords kit, kat
+.INPUTS
+  comma separated list on command line
+.OUTPUTS
+  text
+.NOTES
+  Version:        1.0
+  Author:         dd4495 
+  Creation Date:  August 2016
+#>
+
+    
+    [cmdletbinding(SupportsShouldProcess = $True)]
+    param (
+    [Parameter (Mandatory = $True,ValueFromPipeline = $True,ValueFromPipelineByPropertyName = $True,Position = 0)]
+    [string[]]$BaseWords,
+    [Parameter (Mandatory = $True,ValueFromPipeline = $True,ValueFromPipelineByPropertyName = $True,Position = 1)]
+    [string[]]$Endings
+    )
+
+    
+    $outTable = @()
+    foreach ($word in $BaseWords) {
+    $table = New-Object psobject
+        foreach ($end in $Endings) {
+            $table | Add-Member -MemberType NoteProperty -Name $end -Value "$word$end"
+        } 
+       $outTable += $table 
+    }
+    [pscustomobject] $outTable 
